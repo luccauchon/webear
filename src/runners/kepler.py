a952b1e11819959ce063679647109c6678580461
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2025-03-14 22:08:28 -0400

    Backup

--- src/runners/kepler.py
+++ src/runners/kepler.py
@@ -3,5 +3,5 @@ from trainers.one_day_ahead_binary_classification_rc1 import main as one_day_ahe
 if __name__ == '__main__':
     margin = [-3, -2.5, -2, -1.5, -1, 0, 1, 1.5, 2, 2.5, 3]
     for a_margin in margin:
-        run_id = f"M{margin}_"
+        run_id = f"M{a_margin}_"
         one_day_ahead_binary_classification_rc1({"margin": a_margin, "run_id": run_id})
