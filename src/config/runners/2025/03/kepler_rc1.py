883ea51f1063755f7697fa066d9f4d713bcbca7c
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2025-03-23 21:24:40 -0400

    Backup

--- src/config/runners/2025/03/kepler_rc1.py
+++ src/config/runners/2025/03/kepler_rc1.py
@@ -2,6 +2,6 @@ tav_dates      = ["2024-01-01", "2025-03-08"]
 mes_dates      = ["2025-03-09", "2025-03-15"]
 inf_dates      = ["2025-03-23", "2025-03-29"]
 
-fast_execution_for_debugging = True
+fast_execution_for_debugging = False
 
-debug_level = "DEBUG"
+debug_level = "INFO"
