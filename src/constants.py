465c8db1488182e297d2d1b00c6ce40960e2c36c
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2025-11-20 09:45:57 -0500

    Backup

--- src/constants.py
+++ src/constants.py
@@ -1,24 +1,26 @@
 import os
-
+IS_RUNNING_ON_CASIR = True if 2 == int(os.getenv("ENV_EXEC_CODE__WEBEAR")) else -1
 # Determine base finance data directory based on drive availability
 if os.path.exists('D:') and os.path.isdir('D:'):
-    BASE_UFINANCE_DIR = r"D:\Finance\data\yfinance"
-    BASE_FORCAST_DIR =  r"D:\Finance\data\forecast"
+    BASE_YFINANCE_DIR = r"D:\Finance\data\yfinance"
+    BASE_FORECAST_DIR = r"D:\Finance\data\forecast"
 else:
-    BASE_UFINANCE_DIR = r"C:\Finance\data\yfinance"
-    BASE_FORCAST_DIR  = r"C:\Finance\data\forecast"
-
+    BASE_YFINANCE_DIR = r"C:\Finance\data\yfinance"
+    BASE_FORECAST_DIR  = r"C:\Finance\data\forecast"
+if IS_RUNNING_ON_CASIR:
+    BASE_YFINANCE_DIR = r"/gpfs/groups/gc014b/cj3272/experiences/yfinance"
+    BASE_FORECAST_DIR  = "/gpfs/groups/gc014b/cj3272/experiences/forecast"
 # Ensure the directory exists (optional, but helpful if you're writing later)
-os.makedirs(BASE_UFINANCE_DIR, exist_ok=True)
+os.makedirs(BASE_YFINANCE_DIR, exist_ok=True)
 
 # Define output filenames using the base directory
-FYAHOO__OUTPUTFILENAME       = os.path.join(BASE_UFINANCE_DIR, "snapshot.pkl")
-FYAHOO__OUTPUTFILENAME_DAY   = os.path.join(BASE_UFINANCE_DIR, "snapshot_day.pkl")
-FYAHOO__OUTPUTFILENAME_WEEK  = os.path.join(BASE_UFINANCE_DIR, "snapshot_week.pkl")
-FYAHOO__OUTPUTFILENAME_MONTH = os.path.join(BASE_UFINANCE_DIR, "snapshot_month.pkl")
-FYAHOO__OUTPUTFILENAME_QUARTER = os.path.join(BASE_UFINANCE_DIR, "snapshot_quarter.pkl")
-FYAHOO__OUTPUTFILENAME_YEAR  = os.path.join(BASE_UFINANCE_DIR, "snapshot_year.pkl")
-FYAHOO_TICKER__OUTPUTFILENAME = os.path.join(BASE_UFINANCE_DIR, "snapshot_ticker.pkl")
+FYAHOO__OUTPUTFILENAME       = os.path.join(BASE_YFINANCE_DIR, "snapshot.pkl")
+FYAHOO__OUTPUTFILENAME_DAY   = os.path.join(BASE_YFINANCE_DIR, "snapshot_day.pkl")
+FYAHOO__OUTPUTFILENAME_WEEK  = os.path.join(BASE_YFINANCE_DIR, "snapshot_week.pkl")
+FYAHOO__OUTPUTFILENAME_MONTH = os.path.join(BASE_YFINANCE_DIR, "snapshot_month.pkl")
+FYAHOO__OUTPUTFILENAME_QUARTER = os.path.join(BASE_YFINANCE_DIR, "snapshot_quarter.pkl")
+FYAHOO__OUTPUTFILENAME_YEAR  = os.path.join(BASE_YFINANCE_DIR, "snapshot_year.pkl")
+FYAHOO_TICKER__OUTPUTFILENAME = os.path.join(BASE_YFINANCE_DIR, "snapshot_ticker.pkl")
 
 # Constants
 NB_WORKERS = os.cpu_count()
@@ -35,5 +37,5 @@ MY_TICKERS = [
 TOP10_SP500_TICKERS = ["AMZN", "AAPL", "AVGO", "BRK-B", "GOOG", "GOOGL", "META", "MSFT", "NVDA", "TSLA"]
 
 # Forecast output directory (still assumes D: is preferred)
-OUTPUT_DIR_FOURIER_BASED_STOCK_FORECAST = os.path.join(BASE_FORCAST_DIR, r"Fourier_based_stock_forecast")
+OUTPUT_DIR_FOURIER_BASED_STOCK_FORECAST = os.path.join(BASE_FORECAST_DIR, r"Fourier_based_stock_forecast")
 os.makedirs(OUTPUT_DIR_FOURIER_BASED_STOCK_FORECAST, exist_ok=True)
