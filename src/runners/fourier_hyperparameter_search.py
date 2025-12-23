04dfbc797d2e62a474bcb997cd55de35eee4c41c
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2025-12-22 20:36:53 -0500

    Backup

--- src/runners/fourier_hyperparameter_search.py
+++ src/runners/fourier_hyperparameter_search.py
@@ -42,7 +42,7 @@ def main(args):
 
     experiences, results = [], []
     for n_forecast_length_in_training in (1,3,5,8,9,10,20,30):
-        for n_forecasts in (19,39,69,77,88,111,211,322):  # Nombre de modèles à conserver
+        for n_forecasts in (19,39,69,77):  # Nombre de modèles à conserver
             experiences.append({'n_forecast_length_in_training': n_forecast_length_in_training,
                                 'n_forecasts': n_forecasts})
     for one_experience in tqdm(experiences):
