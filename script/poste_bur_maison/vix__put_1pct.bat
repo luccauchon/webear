129fdb90983f7bb2f3ee08a95cac955bbefc67b0
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-02-20 14:15:24 -0500

    Backup

--- script/poste_bur_maison/vix__put_1pct.bat
+++ script/poste_bur_maison/vix__put_1pct.bat
@@ -17,8 +17,14 @@ for /l %%i in (1, 1, 20) do (
     REM Start a new window, call conda, change directory, and run python
     start "VIX Search %%i" cmd /k "call "%CONDA_ACTIVATE%" && call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python VIX_hyperparameter_search_optuna.py --optimize_target %OPTIMIZE_TARGET% --objective_name %OBJECTIVE_NAME% --timeout %TIMEOUT% --look_ahead %%i"
 
-    REM Delay for 5 seconds to prevent race conditions
-    timeout /t 5 /nobreak >nul
+    REM Calculate sleep time: current index minus 1
+    set /a SLEEP_TIME=%%i-1
+
+    REM Only run timeout if SLEEP_TIME is greater than 0
+    if !SLEEP_TIME! GTR 0 (
+        echo Waiting for !SLEEP_TIME! seconds...
+        timeout /t !SLEEP_TIME! /nobreak >nul
+    )
 )
 
 echo.
