9670d82d11701d26d03cc6d5401e5ad29b6cd9d1
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-02-27 11:46:22 -0500

    Backup

---
+++ script/poste_ireq/roulette/pos_seq_1__f1/roulette__1111_days.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: --- Parameters ---
+set "RANGE=365"
+set "DATASET_ID=day"
+set "TIMEOUT=400000"
+set "CONDA_ENV=PY311"
+set "WORK_DIR=C:\PYCHARMPROJECTS\webear\src\optimizers"
+
+start "Roulette Search %DATASET_ID%" cmd /k "call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python roulette_hyperparameter_search_optuna.py --step_back_range %RANGE% --dataset_id %DATASET_ID% --optimize_target pos_seq_1__f1 --timeout %TIMEOUT%"
+timeout /t 1
+
+pause
