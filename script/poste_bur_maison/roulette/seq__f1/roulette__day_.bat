0d28dce59e0392646c89fd55c1a701834cb1afd6
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-02-28 18:28:27 -0500

    Backup

--- script/poste_bur_maison/roulette/seq__f1/roulette__day_.bat
+++ script/poste_bur_maison/roulette/seq__f1/roulette__day_.bat
@@ -10,7 +10,7 @@ set "CONDA_ACTIVATE=C:\Users\cj3272\AppData\Local\miniconda3\Scripts\activate.ba
 set "CONDA_ENV=PY311"
 set "WORK_DIR=C:\Projets\webear\src\optimizers"
 
-start "Roulette Search" cmd /k "call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python roulette_hyperparameter_search_optuna.py --step_back_range %RANGE% --dataset_id %DATASET_ID% --optimize_target pos_seq__f1 --specific_wanted_class 0 1 2 3 --timeout %TIMEOUT% --epsilon %EPSILON%"
+start "Roulette Search" cmd /k "call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python roulette_hyperparameter_search_optuna.py --step_back_range %RANGE% --dataset_id %DATASET_ID% --optimize_target pos_seq__f1 --specific_wanted_class 0 1 2 --timeout %TIMEOUT% --epsilon %EPSILON%"
 sleep 1
 
 start "Roulette Search" cmd /k "call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python roulette_hyperparameter_search_optuna.py --step_back_range %RANGE% --dataset_id %DATASET_ID% --optimize_target neg_seq__f1 --specific_wanted_class 0 1 2 --timeout %TIMEOUT% --epsilon %EPSILON%"
