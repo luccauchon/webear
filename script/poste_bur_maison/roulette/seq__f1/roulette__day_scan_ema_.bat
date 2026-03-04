f3c25e066d7978f0c16b9ca895b169ea21fd8257
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-03 19:39:47 -0500

    Backup

--- script/poste_bur_maison/roulette/seq__f1/roulette__day_scan_ema_.bat
+++ script/poste_bur_maison/roulette/seq__f1/roulette__day_scan_ema_.bat
@@ -10,9 +10,31 @@ set "CONDA_ACTIVATE=C:\Users\cj3272\AppData\Local\miniconda3\Scripts\activate.ba
 set "CONDA_ENV=PY312"
 set "WORK_DIR=C:\Projets\webear\src\optimizers"
 
-start "Roulette Search" cmd /k "call conda activate %CONDA_ENV% && cd /D "%WORK_DIR%" && python roulette_hyperparameter_search_optuna.py --step_back_range %RANGE% --dataset_id %DATASET_ID% --optimize_target pos_seq__f1 --specific_wanted_class 0 1 2 --timeout %TIMEOUT% --epsilon %EPSILON% --activate_ema_space_search true --max_ema_slots 5 --ema_min 2 --ema_max 20  --ema_step 2  --max_ema_shift_slots 3 --ema_shift_min 1 --ema_shift_max 5 --activate_sma_space_search false --activate_rsi_space_search false --activate_macd_space_search false --activate_vwap_space_search false --add_only_vwap_z_and_vwap_triggers false"
-sleep 1
+start "Roulette Search" cmd /k ^
+    "call conda activate %CONDA_ENV% && ^
+    cd /D "%WORK_DIR%" && ^
+    python roulette_hyperparameter_search_optuna.py ^
+        --step_back_range %RANGE% ^
+        --dataset_id %DATASET_ID% ^
+        --optimize_target pos_seq__f1 ^
+        --specific_wanted_class 0 1 2 ^
+        --timeout %TIMEOUT% ^
+        --epsilon %EPSILON% ^
+        --activate_ema_space_search true ^
+        --max_ema_slots 5 ^
+        --ema_min 2 ^
+        --ema_max 20 ^
+        --ema_step 2 ^
+        --max_ema_shift_slots 3 ^
+        --ema_shift_min 1 ^
+        --ema_shift_max 5 ^
+        --activate_sma_space_search false ^
+        --activate_rsi_space_search false ^
+        --activate_macd_space_search false ^
+        --activate_vwap_space_search false ^
+        --add_only_vwap_z_and_vwap_triggers false"
 
+sleep 1
 
 echo All processes have been dispatched.
 pause
