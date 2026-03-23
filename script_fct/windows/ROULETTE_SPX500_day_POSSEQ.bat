8f96ec049dad3a97db4d1f52e5c57190ed3ba289
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-23 19:02:33 -0400

    Backup

--- script_fct/windows/ROULETTE_SPX500_day_POSSEQ.bat
+++ script_fct/windows/ROULETTE_SPX500_day_POSSEQ.bat
@@ -1,7 +1,6 @@
 @echo off
 call conda activate PY312_HT
-D:
-cd D:\PyCharmProjects\webear\src\crusaders\roulette
+cd ..\..\src\crusaders\roulette
 python GSPC_1lookahead_epsilon001_poseq.py
 pause
 
