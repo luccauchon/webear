593cb50bf056649863995d87db61370b5b9e2869
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-24 15:52:46 -0400

    Backup

--- script_train/windows/mmi/week_2.25percent.bat
+++ script_train/windows/mmi/week_2.25percent.bat
@@ -1,6 +1,6 @@
 @echo off
 call conda activate PY312_HT
-cd ..\..
+cd ..\..\..
 cd runners
 python .\MMI_hyperparameter_search_optuna.py --dataset_id week --step_back_range 9999 --n_trials 33333 --use_ema true --return_threshold_min 0.025 --return_threshold_max 0.025 --sma_period_max 60 --mmi_period_max 60 --lookahead_min 1 --lookahead_max 1 --mmi_trend_max_max 60 --study_name 0_025__week__la1
 pause
