4dd6877fcc3b92964bc501196042305ff016e7e9
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-24 21:30:45 -0400

    Backup

---
+++ script_train/windows/roulette/OPT_ACC.bat
@@ -0,0 +1,20 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..\src\optimizers\roulette
+python .\hyperparameter_search_optuna.py ^
+--step_back_range 1111 ^
+--look_ahead 1 ^
+--dataset_id day ^
+--timeout 500000 ^
+--optimize_target seq__precision ^
+--activate_sma_space_search true ^
+--activate_sma_space_search true ^
+--activate_ema_space_search true ^
+--activate_rsi_space_search true ^
+--activate_macd_space_search true ^
+--activate_vwap_space_search true ^
+--add_only_vwap_z_and_vwap_triggers false ^
+--min_percentage_to_keep_class 4.0 ^
+--storage my_storage_roulette ^
+--study_name opt_accuracy
+pause
