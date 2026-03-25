f8b1eb454757ea987b9cd1454e80470de753f929
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-25 14:37:24 -0400

    Backup

---
+++ script_train/linux/mmi/month_3percent.sh
@@ -0,0 +1,5 @@
+conda activate PY312_HT
+cd ../../..
+cd src/runners
+python MMI_hyperparameter_search_optuna.py --dataset_id month --step_back_range 9999 --n_trials 33333 --use_ema true --return_threshold_min 0.03 --return_threshold_max 0.03 --sma_period_max 60 --mmi_period_max 60 --lookahead_min 1 --lookahead_max 1 --mmi_trend_max_max 100 --study_name 0_03__month__la1
+pause
