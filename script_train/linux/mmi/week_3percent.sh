1f878cf3cf333697d12f5c1f7b99703fbbf17cf6
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-25 16:51:53 -0400

    Backup

---
+++ script_train/linux/mmi/week_3percent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda activate PY312_HT
+cd ../../..
+cd src/runners
+python MMI_hyperparameter_search_optuna.py \
+    --dataset_id week \
+    --step_back_range 99999 \
+    --n_trials 64000 \
+    --use_ema true \
+    --return_threshold_min 0.03 \
+    --return_threshold_max 0.03 \
+    --sma_period_min 1 \
+    --sma_period_max 20 \
+    --mmi_period_max 1 \
+    --mmi_period_max 20 \
+    --mmi_trend_max_min 1 \
+    --mmi_trend_max_max 20 \
+    --lookahead_min 1 \
+    --lookahead_max 1 \
+    --study_name 0_03__month__la1
+read
