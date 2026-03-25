8a01d4c078a73d2587e08b4720b466d4dd352da8
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-25 16:55:23 -0400

    Backup

---
+++ script_fct/windows/MMI_this_week.bat
@@ -0,0 +1,6 @@
+call conda activate PY312_HT
+cd ..\..\src\crusaders\mmi
+python mmi_next_week_at_2_0p.py  --keep_last_step=false
+python mmi_next_week_at_2_25p.py --keep_last_step=false
+python mmi_next_week_at_3_0p.py  --keep_last_step=false
+pause
