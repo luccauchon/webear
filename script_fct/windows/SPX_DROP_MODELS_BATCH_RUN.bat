445134f31f5e2f827c96d71c5c79d2a563be3d2a
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-27 18:05:02 -0400

    Backup

--- script_fct/windows/SPX_DROP_MODELS_BATCH_RUN.bat
+++ script_fct/windows/SPX_DROP_MODELS_BATCH_RUN.bat
@@ -1,3 +1,17 @@
-D:
-cd D:\PyCharmProjects\webear\src\crusaders\SPX_drop
-.\run_them_all.bat
+cd ..\..\src\crusaders\SPX_drop
+call conda activate PY312_HT
+echo *******************************************
+echo SPX_drop_anticipation_model_2
+echo *******************************************
+python SPX_drop_anticipation_model_2.py
+
+echo *******************************************
+echo SPX_drop_anticipation_model_3
+echo *******************************************
+python SPX_drop_anticipation_model_3.py
+
+echo *******************************************
+echo SPX_drop_anticipation_model_4
+echo *******************************************
+python SPX_drop_anticipation_model_4.py
+pause
--- script_fct/windows/TREND_TOP10_SPX.bat
+++ script_fct/windows/TREND_TOP10_SPX.bat
@@ -1,4 +1,5 @@
 @echo off
 call conda activate PY311_HT
-python "D:\PyCharmProjects\webear\src\runners\spx_top10_aligned_trend.py"
+cd ..\..\src\runners
+python spx_top10_aligned_trend.py
 pause
