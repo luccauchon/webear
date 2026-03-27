7583f1a80347f0fe015ec5a178998dd1e0593ff8
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-27 18:11:20 -0400

    Backup

--- script_train/linux/mmi/1day_2percent.sh
+++ script_train/linux/mmi/1day_2percent.sh
@@ -4,12 +4,12 @@ conda activate PY312_HT
 cd ../../..
 cd src/runners
 python MMI_hyperparameter_search_optuna.py \
-    --dataset_id week \
+    --dataset_id day \
     --step_back_range 99999 \
     --n_trials 99999 \
     --use_ema true \
-    --return_threshold_min 0.03 \
-    --return_threshold_max 0.03 \
+    --return_threshold_min 0.02 \
+    --return_threshold_max 0.02 \
     --sma_period_min 1 \
     --sma_period_max 100 \
     --mmi_period_max 1 \
@@ -18,5 +18,5 @@ python MMI_hyperparameter_search_optuna.py \
     --mmi_trend_max_max 100 \
     --lookahead_min 1 \
     --lookahead_max 1 \
-    --study_name 0_03__week__la1
+    --study_name 0_02__day__la1
 read
--- script_train/linux/mmi/2days_2percent.sh
+++ script_train/linux/mmi/2days_2percent.sh
@@ -4,19 +4,19 @@ conda activate PY312_HT
 cd ../../..
 cd src/runners
 python MMI_hyperparameter_search_optuna.py \
-    --dataset_id week \
+    --dataset_id day \
     --step_back_range 99999 \
     --n_trials 99999 \
     --use_ema true \
-    --return_threshold_min 0.03 \
-    --return_threshold_max 0.03 \
+    --return_threshold_min 0.02 \
+    --return_threshold_max 0.02 \
     --sma_period_min 1 \
     --sma_period_max 100 \
     --mmi_period_max 1 \
     --mmi_period_max 100 \
     --mmi_trend_max_min 1 \
     --mmi_trend_max_max 100 \
-    --lookahead_min 1 \
-    --lookahead_max 1 \
-    --study_name 0_03__week__la1
+    --lookahead_min 2 \
+    --lookahead_max 2 \
+    --study_name 0_02__day__la2
 read
