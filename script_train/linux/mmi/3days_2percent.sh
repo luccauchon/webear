fc00c75ee2eee324d64a4aed83aabe2b4838c4f6
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-30 18:54:33 -0400

    Backup

---
+++ script_train/linux/mmi/3days_2percent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda activate PY312_HT
+cd ../../..
+cd src/runners
+python MMI_hyperparameter_search_optuna.py \
+    --dataset_id day \
+    --step_back_range 99999 \
+    --n_trials 99999 \
+    --use_ema true \
+    --return_threshold_min 0.02 \
+    --return_threshold_max 0.02 \
+    --sma_period_min 1 \
+    --sma_period_max 100 \
+    --mmi_period_max 1 \
+    --mmi_period_max 100 \
+    --mmi_trend_max_min 1 \
+    --mmi_trend_max_max 100 \
+    --lookahead_min 3 \
+    --lookahead_max 3 \
+    --study_name 0_02__day__la3 \
+    --storage sqlite:///mmi_optuna_3d200p.db
+read
