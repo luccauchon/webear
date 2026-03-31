0b0ea52edef84800cddf120bc232901482bcbbd6
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-31 17:55:51 -0400

    Backup

--- script_train/windows/vix/1day_call_1pct.bat
+++ script_train/windows/vix/1day_call_1pct.bat
@@ -2,7 +2,7 @@
 call conda activate PY312_HT
 cd ..\..\..
 cd src\runners
-python VIX_hyperparameter_search_optuna.py --objective-name 2026_02_20__1pct_balanced ^
+python .\VIX_hyperparameter_search_optuna.py --objective-name 2026_02_20__1pct_balanced ^
 --optimize-target call ^
 --timeout 200000 ^
 --step-back-range 99999 ^
