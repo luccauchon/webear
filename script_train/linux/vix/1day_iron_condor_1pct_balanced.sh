80ebdfd21f8dc45bcc297db95b4fd47661213f6b
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-31 15:42:21 -0400

    Backup

--- script_train/linux/vix/1day_iron_condor_1pct_balanced.sh
+++ script_train/linux/vix/1day_iron_condor_1pct_balanced.sh
@@ -3,4 +3,11 @@ source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate PY312_HT
 cd ../../..
 cd src/runners
-python .\VIX_hyperparameter_search_optuna.py --objective-name 2026_02_20__1pct_balanced --optimize-target iron_condor --timeout 200000 --step-back-range 99999
+python .\VIX_hyperparameter_search_optuna.py \
+    --objective-name 2026_02_20__1pct_balanced \
+    --optimize-target iron_condor \
+    --timeout 200000 \
+    --step-back-range 99999 \
+    --storage 1day_ic_vix.db \
+    --study-name sn_1day_ic_vix
+read
