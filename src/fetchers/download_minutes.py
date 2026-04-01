718360616e182cc1a5f00378d7fb3e262c87c4ad
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-03-31 20:46:31 -0400

    Backup

--- src/fetchers/download_minutes.py
+++ src/fetchers/download_minutes.py
@@ -8,12 +8,12 @@ import time
 
 def dump_dataframes_to_disk(output_dir=r"D:\Finance\data\daily"):
     _today = pd.Timestamp.now().date()
-    print(f"Running the extraction for SPY/VIX @minutes for  {_today}")
-    df_spy, df_spx, df_vix = get_df_SPY_and_VIX_virgin_at_minutes()
+    print(f"Running the extraction for SPY/SPX/VIX/NDX @minutes for  {_today}")
+    df_spy, df_spx, df_vix, df_ndx = get_df_SPY_and_VIX_virgin_at_minutes()
     df_spy.to_pickle(os.path.join(output_dir, f"{_today}__SPY.pkl"))
     df_spx.to_pickle(os.path.join(output_dir, f"{_today}__SPX.pkl"))
     df_vix.to_pickle(os.path.join(output_dir, f"{_today}__VIX.pkl"))
-
+    df_ndx.to_pickle(os.path.join(output_dir, f"{_today}__NDX.pkl"))
 
 if __name__ == '__main__':
     dump_dataframes_to_disk()
