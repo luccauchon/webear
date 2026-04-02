543bb678bd922bf373e9e33d2c5241b809cc8438
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-02 11:31:48 -0400

    Backup

--- script_train/linux/regime/regime_put_5days.sh
+++ script_train/linux/regime/regime_put_5days.sh
@@ -5,5 +5,8 @@ cd ../../..
 cd src/optimizers/regime
 python regime_switching_optimizer.py --storage-url sqlite:///put_5_days.db \
     --study-name put_5_days \
-    --timeout 430000
+    --timeout 430000 \
+    --spread-type put \
+    --strike-distance 0.03 \
+    --forward-days 5
 read
