0f31deaac2c5de18cdd6b8f6730fe232584c7977
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-02 08:37:41 -0400

    Backup

--- script_fct/windows/MMI/MMI__this_day.bat
+++ script_fct/windows/MMI/MMI__this_day.bat
@@ -1,6 +1,7 @@
 call conda activate PY312_HT
 cd ..\..\..\src\crusaders\mmi
-python mmi_next_day_at_1_0p.py --keep_last_step=false
+python mmi_next_day_at_1_0p.py  --keep_last_step=false
 python mmi_next_day_at_1_25p.py --keep_last_step=false
-python mmi_next_day_at_1_5p.py --keep_last_step=false
+python mmi_next_day_at_1_5p.py  --keep_last_step=false
+python mmi_next_day_at_2_0p.py  --keep_last_step=false
 pause
