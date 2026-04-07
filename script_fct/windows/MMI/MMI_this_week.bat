b8406b571b6873b32eb7e9a8dc10d37e8d4d1891
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-07 13:13:21 -0400

    Backup

--- script_fct/windows/MMI/MMI_this_week.bat
+++ script_fct/windows/MMI/MMI_this_week.bat
@@ -3,4 +3,5 @@ cd ..\..\..\src\crusaders\mmi
 python mmi_next_week_at_2_0p.py  --keep_last_step=false
 python mmi_next_week_at_2_25p.py --keep_last_step=false
 python mmi_next_week_at_3_0p.py  --keep_last_step=false
+python mmi_next_week_at_4_0p.py  --keep_last_step=false
 pause
