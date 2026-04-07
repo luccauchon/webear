bae9800257fc672dccf62c1f142b03655c7b3e28
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-06 20:14:05 -0400

    Backup

---
+++ script_train/windows/roulette/OPT_F1_2pourcent_eps001.bat
@@ -0,0 +1,21 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..\src\optimizers\roulette
+python .\hyperparameter_search_optuna.py ^
+--step_back_range 3333 ^
+--look_ahead 1 ^
+--dataset_id day ^
+--timeout 500000 ^
+--optimize-target seq__f1 ^
+--activate_sma_space_search true ^
+--activate_sma_space_search true ^
+--activate_ema_space_search true ^
+--activate_rsi_space_search true ^
+--activate_macd_space_search true ^
+--activate_vwap_space_search true ^
+--add_only_vwap_z_and_vwap_triggers false ^
+--min_percentage_to_keep_class 2.0 ^
+--epsilon 0.001 ^
+--storage my_storage_roulette_3 ^
+--study_name opt_f1
+pause
