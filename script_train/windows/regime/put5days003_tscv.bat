a8be41107f91cbed890dd379e836b3fcf2a3a7dd
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-16 13:17:24 -0400

    Backup

---
+++ script_train/windows/regime/put5days003_tscv.bat
@@ -0,0 +1,12 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..\src\optimizers\regime
+python .\regime_switching_optimizer_tscv.py ^
+--lookback-years 99999 ^
+--forward-days 5 ^
+--dataset-id week ^
+--strike-distance 0.03 ^
+--timeout 500000 ^
+--spread-type put ^
+--storage-url sqlite:///put5days003_tscv.db
+pause
