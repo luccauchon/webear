802d919dd97fefc47391c0ffaefe12b36ec7a11a
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-18 10:25:57 -0400

    Backup

---
+++ script_train/windows/quantloop/put_credit_spread_0.01.bat
@@ -0,0 +1,13 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..
+cd src\optimizers\qauntloop
+python .\quant_model_trainer.py --dataset .\combined_monthly_macro.data ^
+--target-type soft_higher ^
+--target-percentage 0.01 ^
+--look-ahead 1 ^
+--scaler FunctionTransformer ^
+--estimator RandomForestClassifier ^
+--training-mode random ^
+--time-limit 86400
+pause
