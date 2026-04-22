4b242f66f912d0a44d0b07f689897189335b1b5e
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-22 10:00:26 -0400

    Backup

---
+++ script_fct/windows/QUANT/MONTH_IRON_CONDOR_4PCT.bat
@@ -0,0 +1,10 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..\src\optimizers\quantloop
+echo  python .\quant_model_trainer.py --training-mode random --target-type in_between --target-percentage 0.04 --time-limit 70000
+echo ==========================================================================
+python .\market_and_macro_data_collector.py --output-dir D:\Finance\compiled_models\quant\2026.04.22 --filename generated__combined_monthly_macro.data --freq month
+echo ==========================================================================
+python .\quant_model_trainer.py --real-time --model-path D:\Finance\compiled_models\quant\2026.04.22\best_model_RandomForestClassifier_in_between_20260422_064325.pkl --dataset D:\Finance\compiled_models\quant\2026.04.22\generated__combined_monthly_macro.data
+pause
+
