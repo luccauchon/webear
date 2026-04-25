3b6310c7741f2180755fa33fc146d4e42a0ce2b7
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-25 08:48:52 -0400

    Backup

--- script_train/windows/quantloop/put_credit_spread_1month_0.03.bat
+++ script_train/windows/quantloop/put_credit_spread_1month_0.03.bat
@@ -4,7 +4,7 @@ cd ..\..\..
 cd src\optimizers\quantloop
 python .\quant_model_trainer.py --dataset .\combined_monthly_macro.data ^
 --target-type soft_higher ^
---target-percentage 0.01 ^
+--target-percentage 0.03 ^
 --look-ahead 1 ^
 --scaler FunctionTransformer ^
 --estimator RandomForestClassifier ^
