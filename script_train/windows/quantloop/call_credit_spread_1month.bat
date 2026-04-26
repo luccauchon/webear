0e2b5c80e8a846dc3b89a2701f7904f6c6294b48
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-26 18:48:52 -0400

    Backup

--- script_train/windows/quantloop/call_credit_spread_1month.bat
+++ script_train/windows/quantloop/call_credit_spread_1month.bat
@@ -2,12 +2,13 @@
 call conda activate PY312_HT
 cd ..\..\..
 cd src\optimizers\quantloop
-python .\quant_model_trainer.py --dataset .\combined_month_macro.data ^
+start /low /b /wait python .\quant_model_trainer.py --dataset .\combined_month_macro.data ^
 --target-type soft_lower ^
 --target-percentage 0.01 ^
 --look-ahead 1 ^
 --scaler FunctionTransformer ^
 --estimator RandomForestClassifier ^
 --training-mode random ^
---time-limit 86400
+--time-limit 256000
+echo "MONTHLY 1BAR 1% CALL CREDIT SPREAD"
 pause
--- script_train/windows/quantloop/iron_condor_1month_0.03.bat
+++ script_train/windows/quantloop/iron_condor_1month_0.03.bat
@@ -2,12 +2,13 @@
 call conda activate PY312_HT
 cd ..\..\..
 cd src\optimizers\quantloop
-python .\quant_model_trainer.py --dataset .\combined_month_macro.data ^
+start /low /b /wait python .\quant_model_trainer.py --dataset .\combined_month_macro.data ^
 --target-type in_between ^
 --target-percentage 0.03 ^
 --look-ahead 1 ^
 --scaler FunctionTransformer ^
 --estimator RandomForestClassifier ^
 --training-mode random ^
---time-limit 86400
+--time-limit 256000
+echo "MONTHLY 1BAR 3% IRON CONDOR"
 pause
