242fd38497787efd6ccf3d603536dce3d9823e1e
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-27 14:20:44 -0400

    Backup

---
+++ script_fct/windows/WAVLET_SPX500_WEEKLY_FORECAST_NEXT_4_WEEKS_LOOP.bat
@@ -0,0 +1,14 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\src\runners
+python wavelet_realtime.py ^
+  --ticker "^GSPC" ^
+  --col "Close" ^
+  --verbose False ^
+  --dataset_id "week" ^
+  --n_forecast_length 4 ^
+  --n_forecast_length_in_training 4 ^
+  --thresholds_ep "(0.0175,0.0175)" ^
+  --enable-plot false ^
+  --enable-loop true
+pause
