966d5007d857003e37d94fda784a5c5bc42c1459
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-04-28 17:53:10 -0400

    Backup

--- src/optimizers/autotune/realtime_and_backtest_hyperparameter_search_optuna.py
+++ src/optimizers/autotune/realtime_and_backtest_hyperparameter_search_optuna.py
@@ -422,7 +422,7 @@ def entry(args):
                 else:
                     if saved_model['optimize_metric'] == 'win_rate_4':
                         if last_signal == -1. and saved_model['signal_type'] == 'short':
-                            if rt_win_threshold >= .8
+                            if rt_win_threshold >= .8:
                                 print(f"Last data point is {last_date} @{last_price:.0f}, {saved_model['score']:.2%} chance that price STAY BELOW {last_price * (1 + rt_win_threshold):.0f} until {la_date} ({saved_model['params']['lookahead_bars']}B , {rt_win_threshold:.2%})")
                         else:
                             print(f"{saved_model}")
