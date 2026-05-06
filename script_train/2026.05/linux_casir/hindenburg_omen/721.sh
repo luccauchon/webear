09f1293c9b35583948dc91bf2b1afb5e59b12fbd
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-05-05 20:19:32 -0400

    Backup

--- script_train/2026.05/linux_casir/hindenburg_omen/721.sh
+++ script_train/2026.05/linux_casir/hindenburg_omen/721.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ../../../src/optimizers/hindenburg_omen
+cd ../../../../src/optimizers/hindenburg_omen
 python use_case_generator.py --nb-workers 36 --timeout 80000 --output-dir ~/14b/cj3272/experiences/output_hindenburg_omen/ \
-     --experience-id 791 --forward-days 10 15 20 \
+     --experience-id 721 --forward-days 10 15 20 \
      --thresholds -0.02 -0.03 -0.04 -0.05 -0.06 -0.07 -0.08 -0.09 -0.1 -0.11 -0.12 -0.13 --penalties 0.999 \
      --lookback-years 5 --base-signals simple_ma,hull_ma_cross,supertrend,atr_expansion,bb_extreme
 
