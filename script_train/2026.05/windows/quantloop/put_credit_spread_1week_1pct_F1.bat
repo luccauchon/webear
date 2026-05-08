10dba7b9046d27940894430cd1b906ed86635eac
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-05-08 09:26:55 -0400

    Backup

---
+++ script_train/2026.05/windows/quantloop/put_credit_spread_1week_1pct_F1.bat
@@ -0,0 +1,16 @@
+@echo off
+call conda activate PY312_HT
+cd ..\..\..\..
+cd src\optimizers\quantloop
+start /low /b /wait python .\quant_model_trainer.py --dataset .\combined_week_macro.data ^
+--target-type soft_higher ^
+--target-percentage 0.01 ^
+--look-ahead 1 ^
+--n-test 96 ^
+--scorer F ^
+--scaler FunctionTransformer ^
+--estimator RandomForestClassifier ^
+--training-mode random ^
+--time-limit 256000
+echo "WEEKLY 1BAR 1% PUT CREDIT SPREAD"
+pause
