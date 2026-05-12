f142ebfd3cdfa3e4e20396beacaf0dd2a0d44eeb
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-05-12 09:11:40 -0400

    Backup

--- script_train/2026.05/windows/quantloop/put_credit_spread_1week_1pct__phase_2.bat
+++ script_train/2026.05/windows/quantloop/put_credit_spread_1week_1pct__phase_2.bat
@@ -4,7 +4,7 @@ cd ..\..\..\..
 cd src\optimizers\quantloop
 start /low /b /wait python quant_model_trainer_phase_2.py --training-mode fixed ^
 --features MA_Long RSI_Lag1 VIX ^
---optuna-trials 999 ^
+--optuna-trials 3333 ^
 --target-type soft_higher ^
 --target-percentage 0.01 ^
 --look-ahead 1 ^
