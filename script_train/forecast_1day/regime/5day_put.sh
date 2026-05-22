9e42f3b668a95fdb490c57fe92fb08a0c404286c
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-05-21 20:41:54 -0400

    Backup

--- script_train/forecast_1day/regime/5day_put.sh
+++ script_train/forecast_1day/regime/5day_put.sh
@@ -3,12 +3,12 @@ TIMEOUT_VAL=${1:-80000}
 cd ../../../src/optimizers/regime
 PIDS=()
 
-python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd1.db \
+python regime_switching_optimizer.py --storage-url sqlite:///put_5_daye1.db \
     --study-name put_1_day \
     --timeout "$TIMEOUT_VAL" \
     --spread-type put \
     --strike-distance 0.01 \
-    --forward-days 1 \
+    --forward-days 5 \
     --penalize-invalid-cluster \
     --min-n-in-cluster 35 \
     --min-clusters 5 \
@@ -16,12 +16,12 @@ python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd1.db \
     --lookback-years 99999 &
 PIDS+=($!)
 
-python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd2.db \
+python regime_switching_optimizer.py --storage-url sqlite:///put_5_daye2.db \
     --study-name put_1_day \
     --timeout "$TIMEOUT_VAL" \
     --spread-type put \
     --strike-distance 0.015 \
-    --forward-days 1 \
+    --forward-days 5 \
     --penalize-invalid-cluster \
     --min-n-in-cluster 35 \
     --min-clusters 5 \
@@ -29,12 +29,12 @@ python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd2.db \
     --lookback-years 99999 &
 PIDS+=($!)
 
-python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd3.db \
+python regime_switching_optimizer.py --storage-url sqlite:///put_5_daye3.db \
     --study-name put_1_day \
     --timeout "$TIMEOUT_VAL" \
     --spread-type put \
     --strike-distance 0.02 \
-    --forward-days 1 \
+    --forward-days 5 \
     --penalize-invalid-cluster \
     --min-n-in-cluster 35 \
     --min-clusters 5 \
@@ -42,12 +42,12 @@ python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd3.db \
     --lookback-years 99999 &
 PIDS+=($!)
 
-python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd4.db \
+python regime_switching_optimizer.py --storage-url sqlite:///put_5_daye4.db \
     --study-name put_1_day \
     --timeout "$TIMEOUT_VAL" \
     --spread-type put \
     --strike-distance 0.025 \
-    --forward-days 1 \
+    --forward-days 5 \
     --penalize-invalid-cluster \
     --min-n-in-cluster 35 \
     --min-clusters 5 \
@@ -55,12 +55,12 @@ python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd4.db \
     --lookback-years 99999 &
 PIDS+=($!)
 
-python regime_switching_optimizer.py --storage-url sqlite:///put_5_dayd5.db \
+python regime_switching_optimizer.py --storage-url sqlite:///put_5_daye5.db \
     --study-name put_1_day \
     --timeout "$TIMEOUT_VAL" \
     --spread-type put \
     --strike-distance 0.030 \
-    --forward-days 1 \
+    --forward-days 5 \
     --penalize-invalid-cluster \
     --min-n-in-cluster 35 \
     --min-clusters 5 \
