a288379c16750f0453d55334518b056660e58ced
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-06-02 11:09:42 -0400

    Backup

---
+++ script_train/TaurusEngine/autotune/day_perspective.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start "AutoTune Optuna Day" cmd /K "call conda activate PY312_HT && cd ..\..\..\src\optimizers\autotune && python .\realtime_and_backtest_hyperparameter_search_optuna.py --dataset-id day --lookahead-bars 1 --win-threshold 0.005 --min-signal-density 0.075 --train-ratio 0.7 --optimize hold_floor --n-trials 99999 --timeout 86400 --output-dir day_perspective --storage sqlite:///day_perspective\\day_day.db --study-name dayla1"
