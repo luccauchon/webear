b863d6df7e7abef1434ff6571a4127d6a48ea450
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-06-05 10:57:22 -0400

    Backup

--- script_train/2026.05/casir/oerh/FLOOR__36cores_1day/put_10B_3.0A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh _laucher.sh --trials 44444 --threshold 0.03 --lookahead 10 --metric long_accuracy
--- script_train/2026.05/casir/oerh/FLOOR__36cores_1day/put_10B_5.0A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sh _laucher.sh --trials 44444 --threshold 0.05 --lookahead 10 --metric long_accuracy
