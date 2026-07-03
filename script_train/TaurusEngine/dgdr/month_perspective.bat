0c2eb9cbd045bfa3e6f8a7c03cc2bddd2d793586
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-07-03 13:40:37 -0400

    Backup

--- script_train/TaurusEngine/dgdr/month_perspective.bat
+++ script_train/TaurusEngine/dgdr/month_perspective.bat
@@ -2,19 +2,19 @@
 setlocal enabledelayedexpansion
 
 :: Boucle sur les valeurs de densité de signal
-for %%D in (0.05 0.1 0.2 0.3 0.4 0.5) do (
+for %%D in (0.1 0.3 0.5) do (
 
     :: Extraction propre pour le nom du fichier DB (ex: 0.05 devient 005, 0.1 devient 01)
     set "DENS_SUF=%%D"
     set "DENS_SUF=!DENS_SUF:.=!"
 
     :: Boucle sur les valeurs de put-strike-pct
-    for %%P in (0.99 0.98 0.97 0.96 0.95) do (
+    for %%P in (0.999 0.995 0.99 0.985 0.98 0.975 0.97 0.96 0.95) do (
 
         :: Extraction propre pour le nom du fichier DB (ex: 0.99 devient 099)
         set "STRK_SUF=%%P"
         set "STRK_SUF=!STRK_SUF:.=!"
 
-        start "DGDR Optuna Month-Month" cmd /K "call conda activate PY312_HT && cd ..\..\..\src\optimizers\dgdr && python .\realtime_and_backtest_hyperparameter_search_optuna.py --dataset-id month --lookahead-bars 1 --method final_close --min-signal-density %%D --put-strike-pct %%P --call-strike-pct 1. --n-trials 99999 --timeout 3600 --train-ratio 0.90 --signal-type buy --output-dir month_perspective --optuna-storage sqlite:///month_perspective\\month_month_buy_!STRK_SUF!_dens!DENS_SUF!.db --optuna-study-name doesnotmatter"
+        start "DGDR Optuna Month-Month" cmd /C "call conda activate PY312_HT && cd ..\..\..\src\optimizers\dgdr && python .\realtime_and_backtest_hyperparameter_search_optuna.py --dataset-id month --lookahead-bars 1 --method final_close --min-signal-density %%D --put-strike-pct %%P --call-strike-pct 1. --n-trials 99999 --timeout 3600 --train-ratio 0.90 --signal-type buy --output-dir month_perspective --optuna-storage sqlite:///month_perspective\\month_month_buy_!STRK_SUF!_dens!DENS_SUF!.db --optuna-study-name doesnotmatter"
     )
 )
