ef286b2f19c269404f2d839ffac758f07ec7f62f
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-07-14 09:28:19 -0400

    Backup

--- src/graphics/day_week_month.bat
+++ src/graphics/day_week_month.bat
@@ -2,4 +2,3 @@ call conda activate PY312_HT
 python plotly_close_oriented.py --dataset=day   --limit=36
 python plotly_close_oriented.py --dataset=week  --limit=22
 python plotly_close_oriented.py --dataset=month --limit=10
-pause
