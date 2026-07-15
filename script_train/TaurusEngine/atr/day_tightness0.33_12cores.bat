85e3086a6118ff1cdc86f3c62a8f26f7d5a0f8d3
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-07-15 13:41:24 -0400

    Backup

--- script_train/TaurusEngine/atr/day_tightness0.33_12cores.bat
+++ script_train/TaurusEngine/atr/day_tightness0.33_12cores.bat
@@ -1,6 +1,6 @@
 @echo off
 :: 12 process
 for /L %%i in (3,1,14) do (
-    start "ATR Optuna Day %%i" cmd /C "call conda activate PY312_HT && cd ..\..\..\src\runners && python .\atr_backtesting.py --dataset-id day --step-back-range 1500 --atr-window %%i --tightness-weight 0.33 --n-trials 1500 --n-split 0.8"
+    start "DAY ATR %%i" cmd /C "call conda activate PY312_HT && cd ..\..\..\src\runners && python .\atr_backtesting.py --dataset-id day --step-back-range 1500 --atr-window %%i --tightness-weight 0.33 --n-trials 1500 --n-split 0.8"
 	timeout /t 5 /nobreak
 )
