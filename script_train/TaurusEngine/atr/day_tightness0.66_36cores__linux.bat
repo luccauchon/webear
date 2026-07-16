f58d1985ed3c2afc48387f877fde4ff12d538de1
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-07-15 21:22:35 -0400

    Backup

--- script_train/TaurusEngine/atr/day_tightness0.66_36cores__linux.bat
+++ script_train/TaurusEngine/atr/day_tightness0.66_36cores__linux.bat
@@ -3,17 +3,17 @@
 # Activer la gestion de Conda dans le script
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
-# Boucle de 2 à 25 incluse
-for i in {2..25}
+# Boucle de 2 à 36 incluse
+for i in {2..36}
  eagle-eyedi
 do
     echo "Lancement de DAY ATR $i"
     
     # Active l'environnement et lance le script en arrière-plan
     (
-        conda activate PY312_HT && \
+        echo conda activate PY312_HT
         cd ../../../src/runners && \
-        python ./atr_backtesting.py --dataset-id day --step-back-range 3600 --atr-window "$i" --tightness-weight 0.66 --n-trials 1500 --n-split 0.8 --use-close-for-range
+        python ./atr_backtesting.py --dataset-id day --step-back-range 1800 --atr-window "$i" --tightness-weight 0.66 --n-trials 1500 --n-split 0.8 --use-close-for-range
     ) &
 
     # Pause de 5 secondes entre chaque lancement
