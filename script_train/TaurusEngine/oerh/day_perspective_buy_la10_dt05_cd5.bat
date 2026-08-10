39a8e44b1473dc05056c3c37ea7e971354ae6b9b
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-08-10 11:43:31 -0400

    Backup

--- script_train/TaurusEngine/oerh/day_perspective_buy_la10_dt05_cd5.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-for %%i in (0 0.01 0.02 0.03 0.04 0.05) do (
-    start "OERH %%i" cmd /C "call conda activate PY312_HT && cd ..\..\..\src\optimizers\oerh && python .\realtime_and_backtest_hyperparameter_search_optuna.py --n-trials 99999 --optimize-target buy --timeout 80000 --threshold-pct %%i --dataset-id day --lookahead-bars 10 --plot-sample 1800 --density-target 0.05 --target-type any_half_B --cooldown-bars 5 --output-dir models\day_persective\ --disable-plot-sample"
-    timeout /t 2 /nobreak >nul
-)
