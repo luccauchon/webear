c4bc0dee0ca0a51fb1887ffe81a191030c6281dd
Luc Cauchon <33472062+luccauchon@users.noreply.github.com>
2026-08-20 10:12:35 -0400

    Backup

--- script_fct/windows/C_ATR_PROBABILITY_ZONE_DAY.bat
+++ script_fct/windows/C_ATR_PROBABILITY_ZONE_DAY.bat
@@ -1,6 +1,10 @@
 @echo off
 call conda activate PY312_HT
 cd ..\..\src\runners
-python atr_probability_zone.py --dataset-id day
+python atr_probability_zone.py --dataset-id day --use-close-for-range
+:: Attend 10 secondes et affiche un compte à rebours
+timeout /t 10
+
+python atr_probability_zone.py --dataset-id day --no-use-close-for-range
 :: Attend 10 secondes et affiche un compte à rebours
 timeout /t 10
